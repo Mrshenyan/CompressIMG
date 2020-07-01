d68a4a4f4deb385ce944d411f580afd9916ff71d
mrshendc <mrshendc@gmail.com>
2020-07-01 19:11:02 +0800

    Update CompressIMG_py.py
    文件路径校验

--- CompressIMG_py/Script/CompressIMG_py.py
+++ CompressIMG_py/Script/CompressIMG_py.py
@@ -1,28 +1,38 @@
 import tkinter
+import tkinter.messagebox
 import os
+
+
+def getError(msg):
+    tkinter.messagebox.showerror(title="Error", message=msg)
 
 
 def CheckHasFileOrNot(path):
     if os.path.exists(path):
         print("文件夹存在")
+        Files = os.listdir(path)
+        if Files:
+            print("文件夹中有东西")
+            for k in range(len(Files)):
+                if os.path.split(Files[k])[1]==".png":
+                    print("压缩图片~")
+                else:print("没有图片")
     else:
-        print("文件夹不存在")
+        getError("指定路径不存在")
 
 
 def CheckkeyAndPath(key, path):
-    keyStr = key.get()
-    pathStr = path.get()
     key_regStr = ""
     path_regStr = ""
-    if keyStr == "":
-        print("密钥为空")
+    if key == "":
+        getError("密钥为空")
     else:
-        print(keyStr)
-    if pathStr == "":
-        print("路径为空")
+        print(key)
+    if path == "":
+        getError("路径为空")
     else:
-        CheckHasFileOrNot(path)
         print("校验路径下有没有相关文件")
+        CheckHasFileOrNot(path)
 
 
 def CreateCompent(parentPanel, K_entry, P_entry):
@@ -41,8 +51,7 @@ def CreateCompent(parentPanel, K_entry, P_entry):
     parentPanel.frame2.pack(side="top", anchor="nw")
 
     parentPanel.frame3 = tkinter.Frame(parentPanel)
-    parentPanel.goButton = tkinter.Button(parentPanel.frame3, text="GO!", fg="red", font={"宋体", 40}, width=10,
-                                          command=lambda: CheckkeyAndPath(K_entry, P_entry))
+    parentPanel.goButton = tkinter.Button(parentPanel.frame3, text="GO!", fg="red", font={"宋体", 40}, width=10, command=lambda: CheckkeyAndPath(parentPanel.titleEntry.get(), parentPanel.titleEntry2.get()))
     parentPanel.goButton.pack()
     parentPanel.frame3.pack(side="top", anchor="c")
 
@@ -51,8 +60,14 @@ def CreateCompent(parentPanel, K_entry, P_entry):
 
 def CreatePanel():
     panel = tkinter.Tk()
+    height = 300
+    width = 600
+    screenWidth = panel.winfo_screenwidth()
+    screenHeight = panel.winfo_screenheight()
+    print(screenHeight,screenWidth)
+    aligner = '%dx%d+%d+%d' % (width, height, (screenWidth - width) / 2, (screenHeight - height) / 2)
+    panel.geometry(aligner)
     panel.title("Tinify压缩")
-    panel.geometry("600x300+200+500")
     K_entry = tkinter.Variable()
     P_entry = tkinter.Variable()
     entry1, entry2 = CreateCompent(panel, K_entry, P_entry)
